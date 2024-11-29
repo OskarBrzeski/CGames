569798e832bc9cfa2105a74f8bb74db82ae71199
Oskar Brzeski <oskar.brzeski.github@hotmail.com>
2024-11-29 01:04:34 +0000

    display pieces and some input handling

--- src/tetris/tetris.c
+++ src/tetris/tetris.c
@@ -11,15 +11,181 @@ void
 tetris_new_game(void)
 {
     tetris = (TetrisState) {
-        .grid = (bool) {},
-        .next_pieces = (TetrisPiece) {},
+        .lines_cleared = 0,
+        .current_index = 0,
+        .current_piece = tetris_piece(T),
+        .grid = {},
+        .next_pieces = {},
     };
 }
 
 void
 tetris_run_game(void)
 {
+    tetris_handle_input();
     tetris_render_game();
+}
+
+TetrisPiece
+tetris_piece(TetrisPieceType type)
+{
+    switch (type)
+    {
+    case I:
+        return (TetrisPiece) {
+            .position = {.x = 3, .y = -2},
+            .tiles = {
+                         {{.x = 0, .y = 1},
+                 {.x = 1, .y = 1},
+                 {.x = 2, .y = 1},
+                 {.x = 3, .y = 1}},
+                         {{.x = 2, .y = 0},
+                 {.x = 2, .y = 1},
+                 {.x = 2, .y = 2},
+                 {.x = 2, .y = 3}},
+                         {{.x = 0, .y = 2},
+                 {.x = 1, .y = 2},
+                 {.x = 2, .y = 2},
+                 {.x = 3, .y = 2}},
+                         {{.x = 1, .y = 0},
+                 {.x = 1, .y = 1},
+                 {.x = 1, .y = 2},
+                 {.x = 1, .y = 3}},
+                         }
+        };
+    case O:
+        return (TetrisPiece) {
+            .position = {.x = 3, .y = -3},
+            .tiles = {
+                         {{.x = 1, .y = 1},
+                 {.x = 1, .y = 2},
+                 {.x = 2, .y = 1},
+                 {.x = 2, .y = 2}},
+                         {{.x = 1, .y = 1},
+                 {.x = 1, .y = 2},
+                 {.x = 2, .y = 1},
+                 {.x = 2, .y = 2}},
+                         {{.x = 1, .y = 1},
+                 {.x = 1, .y = 2},
+                 {.x = 2, .y = 1},
+                 {.x = 2, .y = 2}},
+                         {{.x = 1, .y = 1},
+                 {.x = 1, .y = 2},
+                 {.x = 2, .y = 1},
+                 {.x = 2, .y = 2}},
+                         }
+        };
+    case T:
+        return (TetrisPiece) {
+            .position = {.x = 3, .y = -2},
+            .tiles = {
+                         {{.x = 1, .y = 0},
+                 {.x = 0, .y = 1},
+                 {.x = 1, .y = 1},
+                 {.x = 2, .y = 1}},
+                         {{.x = 1, .y = 0},
+                 {.x = 1, .y = 1},
+                 {.x = 2, .y = 1},
+                 {.x = 1, .y = 2}},
+                         {{.x = 0, .y = 1},
+                 {.x = 1, .y = 1},
+                 {.x = 2, .y = 1},
+                 {.x = 1, .y = 2}},
+                         {{.x = 1, .y = 0},
+                 {.x = 0, .y = 1},
+                 {.x = 1, .y = 1},
+                 {.x = 1, .y = 2}},
+                         }
+        };
+    case S:
+        return (TetrisPiece) {
+            .position = {.x = 3, .y = -2},
+            .tiles = {
+                         {{.x = 1, .y = 0},
+                 {.x = 2, .y = 0},
+                 {.x = 0, .y = 1},
+                 {.x = 1, .y = 1}},
+                         {{.x = 1, .y = 0},
+                 {.x = 1, .y = 1},
+                 {.x = 2, .y = 1},
+                 {.x = 2, .y = 2}},
+                         {{.x = 1, .y = 1},
+                 {.x = 2, .y = 1},
+                 {.x = 0, .y = 2},
+                 {.x = 1, .y = 2}},
+                         {{.x = 0, .y = 0},
+                 {.x = 0, .y = 1},
+                 {.x = 1, .y = 1},
+                 {.x = 1, .y = 2}},
+                         }
+        };
+    case Z:
+        return (TetrisPiece) {
+            .position = {.x = 3, .y = -2},
+            .tiles = {
+                         {{.x = 0, .y = 0},
+                 {.x = 1, .y = 0},
+                 {.x = 1, .y = 1},
+                 {.x = 2, .y = 1}},
+                         {{.x = 2, .y = 0},
+                 {.x = 1, .y = 1},
+                 {.x = 2, .y = 1},
+                 {.x = 1, .y = 2}},
+                         {{.x = 0, .y = 1},
+                 {.x = 1, .y = 1},
+                 {.x = 1, .y = 2},
+                 {.x = 2, .y = 2}},
+                         {{.x = 1, .y = 0},
+                 {.x = 0, .y = 1},
+                 {.x = 1, .y = 1},
+                 {.x = 0, .y = 2}},
+                         }
+        };
+    case L:
+        return (TetrisPiece) {
+            .position = {.x = 3, .y = -2},
+            .tiles = {
+                         {{.x = 2, .y = 0},
+                 {.x = 0, .y = 1},
+                 {.x = 1, .y = 1},
+                 {.x = 1, .y = 2}},
+                         {{.x = 1, .y = 0},
+                 {.x = 1, .y = 1},
+                 {.x = 1, .y = 2},
+                 {.x = 2, .y = 2}},
+                         {{.x = 0, .y = 1},
+                 {.x = 1, .y = 1},
+                 {.x = 2, .y = 1},
+                 {.x = 0, .y = 2}},
+                         {{.x = 0, .y = 0},
+                 {.x = 1, .y = 0},
+                 {.x = 1, .y = 1},
+                 {.x = 1, .y = 2}},
+                         }
+        };
+    case J:
+        return (TetrisPiece) {
+            .position = {.x = 3, .y = -2},
+            .tiles = {
+                         {{.x = 0, .y = 0},
+                 {.x = 0, .y = 1},
+                 {.x = 1, .y = 1},
+                 {.x = 2, .y = 1}},
+                         {{.x = 1, .y = 0},
+                 {.x = 2, .y = 0},
+                 {.x = 1, .y = 1},
+                 {.x = 1, .y = 2}},
+                         {{.x = 0, .y = 1},
+                 {.x = 1, .y = 1},
+                 {.x = 2, .y = 1},
+                 {.x = 2, .y = 2}},
+                         {{.x = 1, .y = 0},
+                 {.x = 1, .y = 1},
+                 {.x = 0, .y = 2},
+                 {.x = 1, .y = 2}},
+                         }
+        };
+    }
 }
 
 void
@@ -44,6 +210,7 @@ tetris_render_grid(void)
 
     tetris_render_grid_rows(&grid);
     tetris_render_grid_columns(&grid);
+    tetris_render_pieces(&grid, T);
 }
 
 void
@@ -71,11 +238,66 @@ tetris_render_grid_columns(TetrisGridStuff* grid)
 }
 
 void
-tetris_render_pieces(void)
+tetris_render_pieces(TetrisGridStuff* grid, TetrisPieceType type)
+{
+    TetrisPiece* piece = &tetris.current_piece;
+
+    for (int i = 0; i < 4; i++)
+    {
+        int offset = grid->cell_size + grid->grid_border;
+        int x =
+            grid->margin_x + grid->grid_border +
+            offset * (piece->position.x + piece->tiles[piece->rotation][i].x);
+        int y =
+            grid->margin_y + grid->grid_border +
+            offset * (piece->position.y + piece->tiles[piece->rotation][i].y);
+
+        DrawRectangle(x, y, grid->cell_size, grid->cell_size,
+                      tetris_colour(type));
+    }
+}
+
+Color
+tetris_colour(TetrisPieceType type)
+{
+    switch (type)
+    {
+    case I: return BLUE;
+    case O: return YELLOW;
+    case T: return PURPLE;
+    case S: return GREEN;
+    case Z: return RED;
+    case L: return ORANGE;
+    case J: return DARKBLUE;
+    }
+}
+
+void
+tetris_render_text(TetrisGridStuff* grid)
 {
 }
 
 void
-tetris_render_text(void)
+tetris_handle_input(void)
 {
+    int key;
+    do {
+        key = GetKeyPressed();
+
+        switch (key)
+        {
+        case KEY_X:
+            tetris.current_piece.rotation =
+                (tetris.current_piece.rotation + 1) % 4;
+            break;
+        case KEY_Z:
+            tetris.current_piece.rotation =
+                ((tetris.current_piece.rotation - 1) % 4 + 4) % 4;
+            break;
+        case KEY_LEFT:  --tetris.current_piece.position.x; break;
+        case KEY_RIGHT: ++tetris.current_piece.position.x; break;
+        case KEY_DOWN:  ++tetris.current_piece.position.y;
+        }
+    }
+    while (key != 0);
 }
--- src/tetris/tetris.h
+++ src/tetris/tetris.h
@@ -5,27 +5,38 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+typedef enum {
+    I,
+    O,
+    T,
+    S,
+    Z,
+    L,
+    J,
+} TetrisPieceType;
+
+typedef enum {
+    THREE,
+    FOUR,
+} TetrisSize;
+
 typedef struct {
     int8_t x;
     int8_t y;
 } TetrisTile;
 
-typedef enum {
-    OnTile,
-    BetweenTiles,
-} TetrisCentreType;
-
 typedef struct {
     int8_t rotation;
-    TetrisCentreType centre_type;
-    TetrisTile centre;
-    TetrisTile tiles[4];
+    TetrisTile position;
+    TetrisTile tiles[4][4];
 } TetrisPiece;
 
 typedef struct {
     int16_t lines_cleared;
-    bool grid[20][10];
-    TetrisPiece next_pieces[14];
+    int8_t current_index;
+    TetrisPiece current_piece;
+    TetrisPieceType grid[20][10];
+    TetrisPieceType next_pieces[14];
 } TetrisState;
 
 typedef struct {
@@ -40,13 +51,16 @@ typedef struct {
 void tetris_new_game(void);
 void tetris_run_game(void);
 
+TetrisPiece tetris_piece(TetrisPieceType type);
+
 void tetris_render_game(void);
 
 void tetris_render_grid(void);
 void tetris_render_grid_rows(TetrisGridStuff* grid);
 void tetris_render_grid_columns(TetrisGridStuff* grid);
+void tetris_render_pieces(TetrisGridStuff* grid, TetrisPieceType type);
+Color tetris_colour(TetrisPieceType type);
+void tetris_render_text(TetrisGridStuff* grid);
 
-void tetris_render_pieces(void);
-void tetris_render_text(void);
-
+void tetris_handle_input(void);
 #endif
